c08b69105d2b83bb2c9a99423eceab781b66e61a
shoyeb001 <shoyebjio3398@gmail.com>
2024-07-12 13:26:15 +0530

    fontend proxy config eliminated

--- ecommercefrontend/vite.config.js
+++ ecommercefrontend/vite.config.js
@@ -5,18 +5,18 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
+  // plugins: [react()],
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: "https://ecommerce-sandy-nine.vercel.app/",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 })
