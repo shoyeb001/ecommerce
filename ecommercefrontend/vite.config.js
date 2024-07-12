30f0184e21d1e0879f9330f69c91394235d42f46
shoyeb001 <shoyebjio3398@gmail.com>
2024-07-12 13:39:24 +0530

    layout error solved

--- ecommercefrontend/vite.config.js
+++ ecommercefrontend/vite.config.js
@@ -5,12 +5,12 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //   },
-  // },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // server: {
   //   proxy: {
   //     '/api': {
